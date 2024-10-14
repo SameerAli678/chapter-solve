9dfff64309cdd3b17815978d3e9f5a0051ccd0d3
SameerAli678 <alikhansameer@gmail.com>
2024-10-14 23:46:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+//  ===========chapter 1==========
+// alert
+// alert(`good website \n good done `)
+// compelete chapter 1
+// ============chapter 2==========
+// let name = "sameer ali"
+// alert(name);
+// let age = "17 year old"
+// alert(age);
+// let massge = "Hello world"
+// alert(massge);
+
+// let pizza = " pizza \n pizz \n piz \n pi \n p "
+// alert(pizza)
+// let email = "eexmple@gmail.com"
+// alert(email);
+// let massge2 ="A smarter way to learn JavaScript"
+// alert(massge2);
+
+// let sam = "Hi! iam sameer ali";
+// document.write(sam);
+
+// let alert_browser = "show me";
+
+// alert(alert_browser);
+// let img = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+// alert(img);
+// document.write(img);
+// compelete chapter 2
+//    =========chapter 3===========
+// let age = "i am 17
+
+// let massge = 1;
+
+// function show() {
+//   let sam = `you have visited this site ${massge++} times`;
+//   alert(sam);
+// alert(`You have visited this site  ${massge++}  times`)
+// console.log(`You have visited this site ${massge++} times`);
+// }
+// let words = `<b> jhon Doe</b> ordered <b>5 T-shirt </b>(s) on <b>XYZ</b> clothing store`;
+// document.write(words);
+//  comopelete chapter 3
