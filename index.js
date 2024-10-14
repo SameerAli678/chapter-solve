d8538e75d10c83ca929839ab872090be168fda39
SameerAli678 <alikhansameer@gmail.com>
2024-10-15 02:38:00 +0500

    chapter 4 compelete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //  ===========chapter 1==========
 // alert
 // alert(`good website \n good done `)
-// compelete chapter 1
+// compelete chapter 1.......
 // ============chapter 2==========
 // let name = "sameer ali"
 // alert(name);
@@ -27,8 +27,8 @@
 
 // alert(img);
 // document.write(img);
-// compelete chapter 2
-//    =========chapter 3===========
+// compelete chapter 2.........
+//    =========chapter 3=========== 
 // let age = "i am 17
 
 // let massge = 1;
@@ -41,4 +41,99 @@
 // }
 // let words = `<b> jhon Doe</b> ordered <b>5 T-shirt </b>(s) on <b>XYZ</b> clothing store`;
 // document.write(words);
-//  comopelete chapter 3
+//  compelete chapter 3..........
+//    ==========chapter 4 =============
+//  let sameer = `vs  sameer`
+//  console.log(sameer);
+// let 23 = "24" not possible
+// let $ = "sameer"
+// console.log($);
+// let sameer_ali =" sameer_ali " +"is the best person in the world "
+// console.log(sameer_ali);
+// let $sameer = "sameer ali  " + "is the loyel person in the world for every one! "
+// console.log($sameer);
+        //   5 illgal variable
+// let let = "" illgal (not possible );
+// let alert  =  "" illgal (not possible );
+// let object = "" illgal (not possible );
+// let integer = "" illgal (not possible );
+// let "sameer" = "" illgal (not possible )
+            // 5 ligal variable
+// let sameer = "akash" legal (its possible );
+// let sameer23 =" koch bhi " legal (its possible ); 
+// let watch = " ite a special watch" legal (its possible);
+// let  sameer_ali = "is tarha bhi variable diclear kar sakte he " legal (its possible ); 
+// let $  = " thats Good " legal (its possible );
+        // enough
+// let SameerAli = "camel case"
+// compelete chapter 4 ..........
+    // =============chapter 5================
+    // let a = 3;
+    // let b = 5;
+    // document.write(`sum of 3 and 5 is   ${a +b}`);
+    // console.log(`sum of 3 and 5 is   ${a +b}`);
+    
+    // let c = 3;
+    // let d = 5;
+    // document.write(`sum of 3 and 5 is   ${c *d}`);
+    // console.log(`sum of 3 and 5 is   ${a *b}`);
+    
+    // let e = 3;
+    // let f = 5;
+    // document.write(`sum of 3 and 5 is   ${f /e}`);
+    // console.log(`sum of 3 and 5 is   ${a /b}`);
+    
+    // let g = 3;
+    // let h= 5;
+    // document.write(`sum of 3 and 5 is   ${g - h}`);
+    // console.log(`sum of 3 and 5 is   ${a -b}`);
+    
+    // let i = 3;
+    // let j = 5;
+    // document.write(`sum of 3 and 5 is   ${i **j}`);
+    // console.log(`sum of 3 and 5 is   ${i **j}`);
+    // let a = 6;
+    // let number = 1 + 2+4+5+6
+    // let s = ""
+
+    // const number1 = [`Initial: ${5} increment :${number++} a  `];
+    // console.log(number1);
+    // document.write(number1)
+    
+    // let math = 5
+
+
+    
+        // for (let i = 0;i < number.length ; i++) {
+            //  s += number[i]
+
+            // let math = 5
+            // if(i = 5){
+            //     i++
+            // }asle (
+            //     i--
+            // )
+            // console.log(math);
+            
+           
+            
+        // }
+        // console.log(number);
+        
+        
+    
+    
+
+
+
+
+
+    
+              
+
+
+
+ 
+
+ 
+ 
