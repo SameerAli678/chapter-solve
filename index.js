4479e433286ce63bd14d8419f55ff9c2a7a0f6bc
SameerAli678 <alikhansameer@gmail.com>
2024-10-19 02:44:02 +0500

    chapter 6 Queation no 5

--- index.js
+++ index.js
@@ -28,7 +28,7 @@
 // alert(img);
 // document.write(img);
 // compelete chapter 2.........
-//    =========chapter 3=========== 
+//    =========chapter 3===========
 // let age = "i am 17
 
 // let massge = 1;
@@ -52,88 +52,232 @@
 // console.log(sameer_ali);
 // let $sameer = "sameer ali  " + "is the loyel person in the world for every one! "
 // console.log($sameer);
-        //   5 illgal variable
+//   5 illgal variable
 // let let = "" illgal (not possible );
 // let alert  =  "" illgal (not possible );
 // let object = "" illgal (not possible );
 // let integer = "" illgal (not possible );
 // let "sameer" = "" illgal (not possible )
-            // 5 ligal variable
+// 5 ligal variable
 // let sameer = "akash" legal (its possible );
-// let sameer23 =" koch bhi " legal (its possible ); 
+// let sameer23 =" koch bhi " legal (its possible );
 // let watch = " ite a special watch" legal (its possible);
-// let  sameer_ali = "is tarha bhi variable diclear kar sakte he " legal (its possible ); 
+// let  sameer_ali = "is tarha bhi variable diclear kar sakte he " legal (its possible );
 // let $  = " thats Good " legal (its possible );
-        // enough
+// enough
 // let SameerAli = "camel case"
 // compelete chapter 4 ..........
-    // =============chapter 5================
-    // let a = 3;
-    // let b = 5;
-    // document.write(`sum of 3 and 5 is   ${a +b}`);
-    // console.log(`sum of 3 and 5 is   ${a +b}`);
-    
-    // let c = 3;
-    // let d = 5;
-    // document.write(`sum of 3 and 5 is   ${c *d}`);
-    // console.log(`sum of 3 and 5 is   ${a *b}`);
-    
-    // let e = 3;
-    // let f = 5;
-    // document.write(`sum of 3 and 5 is   ${f /e}`);
-    // console.log(`sum of 3 and 5 is   ${a /b}`);
-    
-    // let g = 3;
-    // let h= 5;
-    // document.write(`sum of 3 and 5 is   ${g - h}`);
-    // console.log(`sum of 3 and 5 is   ${a -b}`);
-    
-    // let i = 3;
-    // let j = 5;
-    // document.write(`sum of 3 and 5 is   ${i **j}`);
-    // console.log(`sum of 3 and 5 is   ${i **j}`);
-    // let a = 6;
-    // let number = 1 + 2+4+5+6
-    // let s = ""
+// =============chapter 5================
+// Question 01
+// let a = 3;
+// let b = 5;
+// document.write(`sum of 3 and 5 is   ${a +b}`);
+// console.log(`sum of 3 and 5 is   ${a +b}`);
+//   Queation 02
 
-    // const number1 = [`Initial: ${5} increment :${number++} a  `];
-    // console.log(number1);
-    // document.write(number1)
-    
-    // let math = 5
+// let c = 3;
+// let d = 5;
+// document.write(`sum of 3 and 5 is   ${c *d}`);
+// console.log(`sum of 3 and 5 is   ${a *b}`);
 
+// let e = 3;
+// let f = 5;
+// document.write(`sum of 3 and 5 is   ${f /e}`);
+// console.log(`sum of 3 and 5 is   ${a /b}`);
 
-    
-        // for (let i = 0;i < number.length ; i++) {
-            //  s += number[i]
+// let g = 3;
+// let h= 5;
+// document.write(`sum of 3 and 5 is   ${g - h}`);
+// console.log(`sum of 3 and 5 is   ${a -b}`);
 
-            // let math = 5
-            // if(i = 5){
-            //     i++
-            // }asle (
-            //     i--
-            // )
-            // console.log(math);
-            
-           
-            
-        // }
-        // console.log(number);
-        
-        
-    
-    
+// let i = 3;
+// let j = 5;
+// document.write(`sum of 3 and 5 is   ${i **j}`);
+// console.log(`sum of 3 and 5 is   ${i **j}`);
+// let a = 6;
+// let number = 1 + 2+4+5+6
+// let s = ""
 
+// const number1 = [`Initial: ${5} increment :${number++} a  `];
+// console.log(number1);
+// document.write(number1)
 
+// let math = 5
 
+// for (let i = 0;i < number.length ; i++) {
+//  s += number[i]
 
+// let math = 5
+// if(i = 5){
+//     i++
+// }asle (
+//     i--
+// )
+// console.log(math);
 
-    
-              
+// }
+// console.log(number);
+//   Question 03
+// let  a;
+// document.write(`“Value after variable declaration is: ${a} <br>`);
+// a=5;
+// document.write(`Initial value:${a} <br> `)
+// a++;
+// document.write(`“Value after increment is: ${a} <br>`)
+// a+=7;
+// document.write(`“Value after addition is: ${a} <br>`)
+// a--;
+// document.write(`Value after decrement is: ${a} <br>` )
+// a %= 3
+// document.write(`“The remainder is : ${a}”`)
+// let ticket = 600;
+// document.write(`Total cost to buy ${5} tickets to a movie is : ${ticket*5} PKR`)
+// Question 06 (a)
+// let a = 4;
+// document.write(`
+//     Table of 4 <br>;
+//       4x1 = ${a * 1}<br>
+//        4x2 = ${a * 2}<br>
+//         4x3 = ${a * 3}<br>
+//          4x4 = ${a * 4}<br>
+//           4x5 = ${a * 5}<br>
+//            4x6 = ${a * 6}<br>
+//             4x7 = ${a * 7}<br>
+//              4x8 = ${a * 8}<br>
+//               4x9 = ${a * 9}<br>
+//                4x10 = ${a * 10}<br>`);
+// Question 05 (b)
 
+// function showtable() {
+//   let number = prompt(`Enter a number `);
 
+//   document.write(`Table 0f ${number}  <br>`);
+//   for (let i = 1; i <= 10; i++) {
+//     document.write(`${number} x ${[i]}  =   ${number * i} <br> `);
+//   }
+// }
+// // showtable();
+// Question 06
+//  var f = prompt();
+//  var c = (f - 32)* 5/9;
+//  document.write(`${f} F is   ${c} C <br>`)
+//  var c = prompt();
+//  var f = (c*9/5) +32;
+//  document.write(`${c} C is   ${f} F <br>`)
 
- 
+//  console.log(f , c);
+//    Question 07
+// let item_1 = 650;
+// let item_2 = 100;
+// let charges = 100;
+// let Quantity_item_1 = prompt(" Quantity of item_1 ");
+// let Quantity_item_2 = prompt(" Quantity of item_2 ");
+// document.write(` Price of item 1 is ${item_1} <br>
+//       Quantity of item_1 is ${Quantity_item_1} <br>
+//        Price of item 2 is ${item_2} <br>
+//        Quantity of item_2 is ${Quantity_item_2} <br>
+//        Shiping charges ${charges} <br>
+//   Total cost of your order is ${
+//     item_1 * Quantity_item_1 + item_2 * Quantity_item_2 + charges
+//   } ..`);
+//   Question 08
 
- 
- 
+// let Total_marks = prompt("Emter a toatl marks ?")
+// let Marks_obtained = prompt( "Enetr a Marks obtained ?")
+// let multiply = 100;
+// document.write(`Total marks : ${Total_marks} <br>
+//     Marks_obtained : ${Marks_obtained} <br>
+//     percentage ${ Marks_obtained /Total_marks* multiply}`)
+//  Question 09
+// function convertPkrToDollar(Pkr) {
+//   const exchangeRate = 104.8; // 1 Dollar = 104.80 pkr
+//   const Dollar = Pkr / exchangeRate;
+//   return Dollar.toFixed(3);
+// }
+// let $ = prompt("Enter the number convert to dollar");
+// let DollarAmount = convertPkrToDollar($)
+// // document.write(DollarAmount);
+
+// function convertPkrToDollar(Pkr) {
+//     const exchangeRate = 28; // 1 Dollar = 104.80 pkr
+//     const riyal = Pkr / exchangeRate;
+//     return riyal.toFixed(3);
+//   }
+//   let Riyal = prompt("Enter the number to riyal");
+//   let riyalAmount = convertPkrToDollar(Riyal)
+//   document.write(riyalAmount+DollarAmount);
+// Question 10;
+//  let number;
+//  number= 0;
+//  document.write(` initialize : ${number} <br>`)
+//  number+=5;
+//  document.write(`Add to : ${number} <br>`)
+//  number*=10;
+//  document.write(`multiply by 10 : ${number} <br>`)
+//  number/=25;
+//  document.write(`Divided to : ${number.toFixed(0)} <br>`)
+// Question 11;
+
+// let birthYear = prompt("Enter Your Brith Year");
+// let currentYear = new Date().getUTCFullYear();
+//   ===========chapter 6 =============
+// Question 01
+// let Result;
+// Result = 10;
+// document.write(`Result: <br>`);
+// document.write(
+//   `The value is :${Result}<br>     .............................. <br>`
+// );
+// ++Result;
+// console.log(Result);
+
+// document.write(
+//   ` The value of ++Result is :${Result} <br> `
+// );
+// document.write(`now the value is : ${Result} <br>`);
+
+// document.write(
+//     ` The value of Result++ is : ${Result} <br> `
+//   );
+// Result++;
+// console.log(Result);
+
+// document.write(`Now the value is : ${Result} <br> `)
+// --Result;
+// document.write(`The value is : ${Result} <br>`)
+
+// document.write(`Now the value is : ${Result} <br>`);
+
+// document.write(`Tha value of Result-- is :${Result} <br>`)
+// Result--;
+// document.write(`now the value is : ${Result}`)
+// Question 02
+
+// let a = 2;
+// let b = 1;
+// let r = --a - --b + ++b + b;
+// console.log(r);
+
+// let result = Result(a+b);
+//  function Result(x,y){
+// return --x - --y + ++y + y;
+//  };
+//  console.log( Result( a,b));
+
+//  document.write(Result(a,b))
+// Question 03
+// const userid = document.getElementById("userid");
+// const massge = document.getElementById("massge");
+// function username() {
+//   if (userid.value === "") return alert("Enter a massge");
+//   massge.innerHTML = `Hi ${userid.value}`;
+// }
+// Question 04
+// const user = document.getElementById("user");
+// const massge = document.getElementById("massge");
+// function usernumber() {
+//   if (user.value === "") return alert("Enter a massge");
+//   else {
+//     massge.innerHTML = ` ${user.value + 5}`;
+//   }
+// }
